5a8633eb723b5ea272b5ce9da6adc34d9ee10b47
Prumixxx <prumixxx@gmail.com>
2021-11-16 11:07:16 +0200

    Initial commit

---
+++ src/test/java/ru/prumix/springshop/SpringShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prumix.springshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
