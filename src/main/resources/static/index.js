c1f5dd6ac72e4345e2c5a7eefc25143c0afbec93
Prumixxx <prumixxx@gmail.com>
2021-12-06 15:27:44 +0200

    HW

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,32 +1,44 @@
-angular.module('app', []).controller('indexController', function ($scope, $http) {
-    const contextPath = 'http://localhost:8080/app';
+angular.module('app', [])
+    .controller('indexController', function ($scope, $http) {
+        const contextPath = 'http://localhost:8080/app';
+        $scope.curPage = 1;
+        $scope.itemsPerPage = 3;
+        $scope.maxSize = 5;
+        $scope.sortKey = 'cost';
+        $scope.sortReverse = false;
 
-    $scope.loadProducts = function () {
-        $http.get(contextPath + '/products')
-            .then(function (response) {
-                $scope.ProductsList = response.data;
-            });
-    };
+        $scope.loadProducts = function () {
+            $http.get(contextPath + '/products')
+                .then(function (response) {
+                    $scope.ProductsList = response.data;
+                });
+        };
 
-    $scope.deleteProduct = function (id) {
-        $http.get(contextPath + '/products/delete/' + id)
-            .then(function (response) {
-                $scope.loadProducts();
-            });
-    }
+        $scope.deleteProduct = function (id) {
+            $http.get(contextPath + '/products/delete/' + id)
+                .then(function (response) {
+                    $scope.loadProducts();
+                });
+        }
 
- /*   $scope.changeCost = function (id, delta) {
-        $http({
-            url: contextPath + '/products/change_cost',
-            method: 'GET',
-            params: {
-                id: id,
-                delta: delta
-            }
-        }).then(function (response) {
-            $scope.loadProducts();
-        });
-    }*/
+        $scope.sort = function (key) {
+            $scope.sortReverse = ($scope.sortKey == key) ? !$scope.sortReverse : $scope.sortReverse;
+            $scope.sortKey = key;
+        }
 
-    $scope.loadProducts();
-});
+
+        /*   $scope.changeCost = function (id, delta) {
+               $http({
+                   url: contextPath + '/products/change_cost',
+                   method: 'GET',
+                   params: {
+                       id: id,
+                       delta: delta
+                   }
+               }).then(function (response) {
+                   $scope.loadProducts();
+               });
+           }*/
+
+        $scope.loadProducts();
+    });
