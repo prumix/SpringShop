0d9a4a3305a25890e2bdbd98db79cfcaf93a302e
prumix <71814019+prumix@users.noreply.github.com>
2021-12-28 16:45:24 +0200

    Delete AppConfig.java

--- src/main/java/ru/prumix/springshop/configs/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.prumix.springshop.configs;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("secrets.properties")
-public class AppConfig {
-}
