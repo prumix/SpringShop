ed44f752f580e1ca5a5bd8124365687fcdfb3226
Prumixxx <prumixxx@gmail.com>
2022-01-14 15:22:15 +0200

    add_redis

--- src/main/java/ru/prumix/springshop/controllers/CartsController.java
+++ src/main/java/ru/prumix/springshop/controllers/CartsController.java
@@ -2,31 +2,96 @@ package ru.prumix.springshop.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.prumix.springshop.dto.Cart;
+import ru.prumix.springshop.dto.StringResponse;
+import ru.prumix.springshop.entities.Product;
+import ru.prumix.springshop.exceptions.ResourceNotFoundException;
 import ru.prumix.springshop.services.CartService;
+import ru.prumix.springshop.services.ProductsService;
+
+import java.security.Principal;
+import java.util.UUID;
+import java.util.function.Consumer;
 
 @RestController
 @RequestMapping("/api/v1/cart")
 @RequiredArgsConstructor
 public class CartsController {
     private final CartService cartService;
+    private final ProductsService productsService;
 
-    @GetMapping
-    public Cart getCurrentCart() {
-        return cartService.getCurrentCart();
+    @GetMapping("/{uuid}")
+    public Cart getCart(Principal principal, @PathVariable String uuid) {
+        String username = null;
+        if (principal != null) {
+            username = principal.getName();
+        }
+        return cartService.getCurrentCart(getCurrentCartUuid(username, uuid));
     }
 
-    @GetMapping("/add/{id}")
-    public void addProductToCart(@PathVariable Long id) {
-        cartService.addProductByIdToCart(id);
+    @GetMapping("/generate")
+    public StringResponse getCart() {
+        return new StringResponse(cartService.generateCartUuid());
     }
 
-    @GetMapping("/clear")
-    public void clearCart() {
-        cartService.getCurrentCart().clear();
+    @GetMapping("/{uuid}/add/{productId}")
+    public void add(Principal principal, @PathVariable String uuid, @PathVariable Long productId) {
+        String username = null;
+        if (principal != null) {
+            username = principal.getName();
+        }
+        cartService.addToCart(getCurrentCartUuid(username, uuid), productId);
+    }
+
+    @GetMapping("/{uuid}/decrement/{productId}")
+    public void decrement(Principal principal, @PathVariable String uuid, @PathVariable Long productId) {
+        String username = null;
+        if (principal != null) {
+            username = principal.getName();
+        }
+        cartService.decrementItem(getCurrentCartUuid(username, uuid), productId);
+    }
+
+    @GetMapping("/{uuid}/remove/{productId}")
+    public void remove(Principal principal, @PathVariable String uuid, @PathVariable Long productId) {
+        String username = null;
+        if (principal != null) {
+            username = principal.getName();
+        }
+        cartService.removeItemFromCart(getCurrentCartUuid(username, uuid), productId);
+    }
+
+    @GetMapping("/{uuid}/clear")
+    public void clear(Principal principal, @PathVariable String uuid) {
+        String username = null;
+        if (principal != null) {
+            username = principal.getName();
+        }
+        cartService.clearCart(getCurrentCartUuid(username, uuid));
+    }
+
+    @GetMapping("/{uuid}/merge")
+    public void merge(Principal principal, @PathVariable String uuid) {
+        String username = null;
+        if (principal != null) {
+            username = principal.getName();
+        }
+        cartService.merge(
+                getCurrentCartUuid(username, null),
+                getCurrentCartUuid(null, uuid)
+        );
+    }
+
+    private String getCurrentCartUuid(String username, String uuid) {
+        if (username != null) {
+            return cartService.getCartUuidFromSuffix(username);
+        }
+        return cartService.getCartUuidFromSuffix(uuid);
     }
 }
--- src/main/resources/static/orders/orders.js
+++ src/main/resources/static/orders/orders.js
@@ -1,12 +1,10 @@
-angular.module('market-front').controller('orderController', function ($scope, $http) {
+angular.module('market-front').controller('ordersController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/app/';
-
 
     $scope.loadOrders = function () {
         $http.get(contextPath + 'api/v1/orders')
             .then(function (response) {
                 $scope.MyOrders = response.data;
-                console.log(response)
             });
     }
 
